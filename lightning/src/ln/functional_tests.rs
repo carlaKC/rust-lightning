593b97a83406732b6e568a047152b32783707256
Carla Kirk-Cohen <kirkcohenc@gmail.com>
2025-05-19 15:18:21 -0400

    t: add coverage for max htlcs for zero fee channels

--- lightning/src/ln/functional_tests.rs
+++ lightning/src/ln/functional_tests.rs
@@ -264,7 +264,8 @@ fn test_insane_zero_fee_channel_open() {
 
 	let chanmon_cfgs = create_chanmon_cfgs(2);
 	let node_cfgs = create_node_cfgs(2, &chanmon_cfgs);
-	let node_chanmgrs = create_node_chanmgrs(2, &node_cfgs, &[Some(cfg.clone()), Some(cfg.clone())]);
+	let node_chanmgrs =
+		create_node_chanmgrs(2, &node_cfgs, &[Some(cfg.clone()), Some(cfg.clone())]);
 	let nodes = create_network(2, &node_cfgs, &node_chanmgrs);
 
 	let node_a_id = nodes[0].node.get_our_node_id();
@@ -283,22 +284,37 @@ fn test_insane_zero_fee_channel_open() {
 
 			let events = nodes[1].node.get_and_clear_pending_events();
 			match events[0] {
-				Event::OpenChannelRequest { temporary_channel_id, .. } =>
-				match nodes[1].node.accept_inbound_channel(&temporary_channel_id, &nodes[0].node.get_our_node_id(), 23, None){
-					Ok(_) => panic!("Unexpected successful channel accept"),
-					Err(e) => assert!(format!("{:?}", e).contains(expected_error_str)),
-				}
-			_ => panic!("Unexpected event"),
+				Event::OpenChannelRequest { temporary_channel_id, .. } => {
+					match nodes[1].node.accept_inbound_channel(
+						&temporary_channel_id,
+						&nodes[0].node.get_our_node_id(),
+						23,
+						None,
+					) {
+						Ok(_) => panic!("Unexpected successful channel accept"),
+						Err(e) => assert!(format!("{:?}", e).contains(expected_error_str)),
+					}
+				},
+				_ => panic!("Unexpected event"),
 			}
-	};
+
+			let events = nodes[1].node.get_and_clear_pending_msg_events();
+			assert_eq!(events.len(), 1);
+			assert!(matches!(events[0], MessageSendEvent::HandleError { .. }));
+		};
 
 	insane_open_helper(
-			"max_accepted_htlcs was 115. It must not be larger than 114".into(),
-			|mut msg| {
-				msg.common_fields.max_accepted_htlcs = 115;
-				msg
-			}
+		"max_accepted_htlcs was 115. It must not be larger than 114".into(),
+		|mut msg| {
+			msg.common_fields.max_accepted_htlcs = 115;
+			msg
+		},
 	);
+
+	insane_open_helper("Zero Fee Channels must never attempt to use a fee".into(), |mut msg| {
+		msg.common_fields.commitment_feerate_sat_per_1000_weight = 123;
+		msg
+	});
 }
 
 #[xtest(feature = "_externalize_tests")]

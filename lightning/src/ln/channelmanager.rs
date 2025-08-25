1370dee0c653968ec9d1617a9ec63a6a6f2b73a0
Maurice <mpch@hey.com>
2025-08-25 14:31:45 -0400

    ln: add trampoline routing payment claiming
    Implement payment claiming for `HTLCSource::TrampolineForward` by
    iterating through previous hop data and claiming funds for each
    HTLC.
    
    Note that testing should be implemented when trampoline forwarding is
    completed.
    
    Co-authored-by: Arik Sosman <git@arik.io>
    Co-authored-by: Maurice Poirrier <mpch@hey.com>

--- lightning/src/ln/channelmanager.rs
+++ lightning/src/ln/channelmanager.rs
@@ -9517,7 +9517,26 @@ This indicates a bug inside LDK. Please report this error at https://github.com/
 					send_timestamp,
 				);
 			},
-			HTLCSource::TrampolineForward { .. } => todo!(),
+			HTLCSource::TrampolineForward { previous_hop_data, .. } => {
+				for current_previous_hop_data in previous_hop_data {
+					self.claim_funds_from_hop_htlc_forward(
+						payment_preimage,
+						forwarded_htlc_value_msat,
+						skimmed_fee_msat,
+						from_onchain,
+						startup_replay,
+						next_channel_counterparty_node_id,
+						next_channel_outpoint,
+						next_channel_id,
+						next_user_channel_id,
+						current_previous_hop_data,
+						// Clone the attribution data because it is going to be updated using the
+						// current_previous_hop_data respectively.
+						attribution_data.clone(),
+						send_timestamp,
+					);
+				}
+			},
 		}
 	}
 

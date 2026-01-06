cf84151d15df23278b3540429bf2660c891b9c9a
Carla Kirk-Cohen <kirkcohenc@gmail.com>
2026-01-06 15:28:42 -0500

    events: add TrampolineForward variant to HTLCHandlingFailureType

--- lightning/src/events/mod.rs
+++ lightning/src/events/mod.rs
@@ -576,6 +576,14 @@ pub enum HTLCHandlingFailureType {
 		/// The payment hash of the payment we attempted to process.
 		payment_hash: PaymentHash,
 	},
+	/// We were responsible for pathfinding and forwarding of a trampoline payment, but failed to
+	/// do so. An example of such an instance is when we can't find a route to the specified
+	/// trampoline destination.
+	TrampolineForward {
+		/// The set of HTLCs dispatched by our node in an attempt to complete the trampoline forward
+		/// which have failed.
+		attempted_htlcs: Vec<HTLCLocator>,
+	},
 }
 
 impl_writeable_tlv_based_enum_upgradable!(HTLCHandlingFailureType,
@@ -592,6 +600,9 @@ impl_writeable_tlv_based_enum_upgradable!(HTLCHandlingFailureType,
 	(3, InvalidOnion) => {},
 	(4, Receive) => {
 		(0, payment_hash, required),
+	},
+	(5, TrampolineForward) => {
+		(1, attempted_htlcs, required_vec),
 	},
 );
 

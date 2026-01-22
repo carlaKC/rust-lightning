7746c1a4aa2611a457b05cb1312fe2bce06d270d
Carla Kirk-Cohen <kirkcohenc@gmail.com>
2026-01-22 13:40:35 -0500

    [wip]: add pending_trampoline_forwards to accumulate inbound MPP
    When we are a trampoline router, we need to accumulate incoming HTLCs
    (if MPP is used) before forwarding the trampoline-routed outgoing
    HTLC(s). This commits adds a new map in channel manager, and mimics the
    handling done for claimable_payments.
    
    This map is not placed in claimable_payments because we'll need to
    be able to lock pending_outbound_payments in the commits that follow
    while holding a lock on our set of trampoline payments (which is not
    possible with claimable_payments).
    
    - [ ] Need to add persistence of trampoline payments

--- lightning/src/ln/channelmanager.rs
+++ lightning/src/ln/channelmanager.rs
@@ -2832,6 +2832,12 @@ pub struct ChannelManager<
 	/// See `ChannelManager` struct-level documentation for lock order requirements.
 	claimable_payments: Mutex<ClaimablePayments>,
 
+	/// The sets of trampoline payments which are in the process of being accumulated on inbound
+	/// channel(s).
+	///
+	/// Note: Not adding ChannelMangaer struct level docs because 4300 removes it.
+	awaiting_trampoline_forwards: Mutex<HashMap<PaymentHash, ClaimablePayment>>,
+
 	/// The set of outbound SCID aliases across all our channels, including unconfirmed channels
 	/// and some closed channels which reached a usable state prior to being closed. This is used
 	/// only to avoid duplicates, and is not persisted explicitly to disk, but rebuilt from the
@@ -3888,6 +3894,7 @@ where
 			forward_htlcs: Mutex::new(new_hash_map()),
 			decode_update_add_htlcs: Mutex::new(new_hash_map()),
 			claimable_payments: Mutex::new(ClaimablePayments { claimable_payments: new_hash_map(), pending_claiming_payments: new_hash_map() }),
+			awaiting_trampoline_forwards: Mutex::new(new_hash_map()),
 			pending_intercepted_htlcs: Mutex::new(new_hash_map()),
 			short_to_chan_info: FairRwLock::new(new_hash_map()),
 
@@ -8617,6 +8624,52 @@ where
 					true
 				},
 			);
+
+			self.awaiting_trampoline_forwards.lock().unwrap().retain(|payment_hash, payment| {
+				if let OnionPayload::Trampoline { incoming_trampoline_shared_secret, .. } =
+					payment.htlcs[0].onion_payload
+				{
+					let htlc_total_msat: u64 =
+						payment.htlcs.iter().map(|h| h.sender_intended_value).sum();
+					let mpp_timeout = check_mpp_timeout(htlc_total_msat, &mut payment.htlcs);
+					if mpp_timeout {
+						let (prev_hops, attempted_htlcs): (Vec<_>, Vec<_>) = payment
+							.htlcs
+							.drain(..)
+							.map(|claimable| {
+								let locator = events::HTLCLocator {
+									channel_id: claimable.prev_hop.channel_id,
+									user_channel_id: claimable.prev_hop.user_channel_id,
+									node_id: claimable.prev_hop.counterparty_node_id,
+								};
+								(claimable.prev_hop, locator)
+							})
+							.unzip();
+
+						timed_out_mpp_htlcs.push((
+							HTLCSource::TrampolineForward {
+								previous_hop_data: prev_hops,
+								incoming_trampoline_shared_secret,
+								outbound_payment: None,
+								// TODO: this should be inside of outbound payment?
+								session_priv: SecretKey::from_slice(
+									&self.entropy_source.get_secure_random_bytes(),
+								)
+								.unwrap(),
+							},
+							*payment_hash,
+							HTLCHandlingFailureType::TrampolineForward { attempted_htlcs },
+						));
+					}
+					!mpp_timeout
+				} else {
+					debug_assert!(
+						false,
+						"awaiting_trampoline_forwards should only contain trampolines"
+					);
+					true
+				}
+			});
 
 			for (htlc_source, payment_hash, failure_type) in timed_out_mpp_htlcs.drain(..) {
 				let failure_reason = LocalHTLCFailureReason::MPPTimeout;
@@ -15183,6 +15236,57 @@ where
 				},
 			);
 
+			self.awaiting_trampoline_forwards.lock().unwrap().retain(|payment_hash, payment| {
+				if let OnionPayload::Trampoline { incoming_trampoline_shared_secret, .. } =
+					payment.htlcs[0].onion_payload
+				{
+					let htlc_timed_out = payment
+						.htlcs
+						.iter()
+						.any(|htlc| htlc.check_onchain_timeout(height, HTLC_FAIL_BACK_BUFFER));
+					if htlc_timed_out {
+						let (previous_hop_data, attempted_htlcs): (Vec<_>, Vec<_>) = payment
+							.htlcs
+							.drain(..)
+							.map(|claimable| {
+								let locator = events::HTLCLocator {
+									channel_id: claimable.prev_hop.channel_id,
+									user_channel_id: claimable.prev_hop.user_channel_id,
+									node_id: claimable.prev_hop.counterparty_node_id,
+								};
+								(claimable.prev_hop, locator)
+							})
+							.unzip();
+
+						timed_out_htlcs.push((
+							HTLCSource::TrampolineForward {
+								previous_hop_data,
+								incoming_trampoline_shared_secret,
+								outbound_payment: None,
+								// TODO: this should be inside of outbound payment?
+								session_priv: SecretKey::from_slice(
+									&self.entropy_source.get_secure_random_bytes(),
+								)
+								.unwrap(),
+							},
+							payment_hash.clone(),
+							HTLCFailReason::reason(
+								LocalHTLCFailureReason::CLTVExpiryTooSoon,
+								vec![],
+							),
+							HTLCHandlingFailureType::TrampolineForward { attempted_htlcs },
+						));
+					}
+					!htlc_timed_out
+				} else {
+					debug_assert!(
+						false,
+						"awaiting_trampoline_forwards should only contain trampolines"
+					);
+					true
+				}
+			});
+
 			let mut intercepted_htlcs = self.pending_intercepted_htlcs.lock().unwrap();
 			intercepted_htlcs.retain(|_, htlc| {
 				if height >= htlc.forward_info.outgoing_cltv_value - HTLC_FAIL_BACK_BUFFER {
@@ -17055,6 +17159,8 @@ where
 			htlc_onion_fields.push(&payment.onion_fields);
 		}
 
+		// TODO: write pending_trampoline_forwards
+
 		let mut monitor_update_blocked_actions_per_peer = None;
 		let mut peer_states = Vec::new();
 		for (_, peer_state_mutex) in per_peer_state.iter() {
@@ -17948,7 +18054,7 @@ where
 			}
 			claimable_htlcs_list.push((payment_hash, previous_hops));
 		}
-
+		// TODO: read pending_trampoline_forwards
 		let peer_count: u64 = Readable::read(reader)?;
 		for _ in 0..peer_count {
 			let peer_pubkey: PublicKey = Readable::read(reader)?;
@@ -19033,6 +19139,7 @@ where
 				claimable_payments,
 				pending_claiming_payments: pending_claiming_payments.unwrap(),
 			}),
+			awaiting_trampoline_forwards: Mutex::new(new_hash_map()),
 			outbound_scid_aliases: Mutex::new(outbound_scid_aliases),
 			short_to_chan_info: FairRwLock::new(short_to_chan_info),
 			fake_scid_rand_bytes: fake_scid_rand_bytes.unwrap(),

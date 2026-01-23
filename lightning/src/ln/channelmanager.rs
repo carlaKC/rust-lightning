f9f8003f21bf2c67c0bfb44a29e67124dca29c94
Carla Kirk-Cohen <kirkcohenc@gmail.com>
2026-01-23 13:44:52 -0500

    fixup! [wip]: add pending_trampoline_forwards to accumulate inbound MPP

--- lightning/src/ln/channelmanager.rs
+++ lightning/src/ln/channelmanager.rs
@@ -15380,11 +15380,6 @@ where
 								previous_hop_data,
 								incoming_trampoline_shared_secret,
 								outbound_payment: None,
-								// TODO: this should be inside of outbound payment?
-								session_priv: SecretKey::from_slice(
-									&self.entropy_source.get_secure_random_bytes(),
-								)
-								.unwrap(),
 							},
 							payment_hash.clone(),
 							HTLCFailReason::reason(

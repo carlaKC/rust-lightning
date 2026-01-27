1bb232cc32ed9ecb52aa6f98f2ceb6dbf7577e64
Carla Kirk-Cohen <kirkcohenc@gmail.com>
2026-01-27 13:49:35 -0500

    ln: store incoming mpp data in PendingHTLCRouting
    When we receive a trampoline forward, we need to wait for MPP parts to
    arrive at our node before we can forward the outgoing payment onwards.
    This commit threads this information through to our pending htlc struct
    which we'll use to validate the parts we receive.

--- lightning/src/ln/onion_payment.rs
+++ lightning/src/ln/onion_payment.rs
@@ -113,6 +113,7 @@ enum RoutingInfo {
 		next_hop_hmac: [u8; 32],
 		shared_secret: SharedSecret,
 		current_path_key: Option<PublicKey>,
+		incoming_multipath_data: Option<msgs::FinalOnionHopData>,
 	},
 }
 
@@ -161,14 +162,15 @@ pub(super) fn create_fwd_pending_htlc_info(
 				reason: LocalHTLCFailureReason::InvalidOnionPayload,
 				err_data: Vec::new(),
 			}),
-		onion_utils::Hop::TrampolineForward { next_trampoline_hop_data, next_trampoline_hop_hmac, new_trampoline_packet_bytes, trampoline_shared_secret, .. } => {
+		onion_utils::Hop::TrampolineForward { outer_hop_data, next_trampoline_hop_data, next_trampoline_hop_hmac, new_trampoline_packet_bytes, trampoline_shared_secret, .. } => {
 			(
 				RoutingInfo::Trampoline {
 					next_trampoline: next_trampoline_hop_data.next_trampoline,
 					new_packet_bytes: new_trampoline_packet_bytes,
 					next_hop_hmac: next_trampoline_hop_hmac,
 					shared_secret: trampoline_shared_secret,
-					current_path_key: None
+					current_path_key: None,
+					incoming_multipath_data: outer_hop_data.multipath_trampoline_data,
 				},
 				next_trampoline_hop_data.amt_to_forward,
 				next_trampoline_hop_data.outgoing_cltv_value,
@@ -194,7 +196,8 @@ pub(super) fn create_fwd_pending_htlc_info(
 					new_packet_bytes: new_trampoline_packet_bytes,
 					next_hop_hmac: next_trampoline_hop_hmac,
 					shared_secret: trampoline_shared_secret,
-					current_path_key: outer_hop_data.current_path_key
+					current_path_key: outer_hop_data.current_path_key,
+					incoming_multipath_data: outer_hop_data.multipath_trampoline_data,
 				},
 				amt_to_forward,
 				outgoing_cltv_value,
@@ -227,7 +230,7 @@ pub(super) fn create_fwd_pending_htlc_info(
 					}),
 			}
 		}
-		RoutingInfo::Trampoline { next_trampoline, new_packet_bytes, next_hop_hmac, shared_secret, current_path_key } => {
+		RoutingInfo::Trampoline { next_trampoline, new_packet_bytes, next_hop_hmac, shared_secret, current_path_key, incoming_multipath_data: multipath_trampoline_data } => {
 			let next_trampoline_packet_pubkey = match next_packet_pubkey_opt {
 				Some(Ok(pubkey)) => pubkey,
 				_ => return Err(InboundHTLCErr {
@@ -254,7 +257,8 @@ pub(super) fn create_fwd_pending_htlc_info(
 						failure: intro_node_blinding_point
 							.map(|_| BlindedFailure::FromIntroductionNode)
 							.unwrap_or(BlindedFailure::FromBlindedNode),
-					})
+					}),
+				incoming_multipath_data: multipath_trampoline_data,
 			}
 		}
 	};

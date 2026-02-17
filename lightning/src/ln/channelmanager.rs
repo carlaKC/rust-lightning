f2feb21a3a7b35e6b13edf5625d3745323841abe
Carla Kirk-Cohen <kirkcohenc@gmail.com>
2026-02-17 14:13:31 +0200

    fixup! [wip]: add awaiting_trampoline_forwards to accumulate inbound MPP

--- lightning/src/ln/channelmanager.rs
+++ lightning/src/ln/channelmanager.rs
@@ -9037,6 +9037,10 @@ impl<
 			);
 
 			self.awaiting_trampoline_forwards.lock().unwrap().retain(|payment_hash, payment| {
+				if payment.htlcs.is_empty() {
+					debug_assert!(false);
+					return false;
+				}
 				if let OnionPayload::Trampoline { .. } = payment.htlcs[0].onion_payload {
 					let htlc_total_msat: u64 =
 						payment.htlcs.iter().map(|h| h.sender_intended_value).sum();

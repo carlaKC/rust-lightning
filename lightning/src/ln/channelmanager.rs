6011384b3476622d2ae3e8150a9cb9e3fa6b2904
Carla Kirk-Cohen <kirkcohenc@gmail.com>
2026-03-18 08:15:41 -0400

    ln/test: add tests for mpp accumulation of trampoline forwards

--- lightning/src/ln/channelmanager.rs
+++ lightning/src/ln/channelmanager.rs
@@ -524,7 +524,7 @@ pub enum BlindedFailure {
 }
 
 #[derive(PartialEq, Eq)]
-enum OnionPayload {
+pub(crate) enum OnionPayload {
 	/// Indicates this incoming onion payload is for the purpose of paying an invoice.
 	Invoice {
 		/// This is only here for backwards-compatibility in serialization, in the future it can be
@@ -5858,6 +5858,20 @@ impl<
 		&self, payment_id: PaymentId, new_payment_metadata: Option<Vec<u8>>,
 	) {
 		self.pending_outbound_payments.test_set_payment_metadata(payment_id, new_payment_metadata);
+	}
+
+	#[cfg(test)]
+	pub(crate) fn test_handle_trampoline_htlc(
+		&self, mpp_part: MppPart, onion_fields: RecipientOnionFields, payment_hash: PaymentHash,
+		next_hop_info: NextTrampolineHopInfo, next_node_id: PublicKey,
+	) -> Result<(), (HTLCSource, onion_utils::HTLCFailReason)> {
+		self.handle_trampoline_htlc(
+			mpp_part,
+			onion_fields,
+			payment_hash,
+			next_hop_info,
+			next_node_id,
+		)
 	}
 
 	/// Pays a [`Bolt11Invoice`] associated with the `payment_id`. See [`Self::send_payment`] for more info.
